e3bbf74ab5fca09190d3700ae9f2b179e13d3c2c
Kevin Johr <kevin.johr@posteo.de>
2021-12-23 22:32:15 +0100

    first quiz and js code added

---
+++ script/question.js
@@ -0,0 +1,90 @@
+let historyQuestions = [
+    {
+        "question": "In welchem Jahr Begann der Zweite Weltkrieg?",
+        "answer_1": "1939",
+        "answer_2": "1914",
+        "answer_3": "1945",
+        "answer_4": "1960",
+        "right": 1
+    },
+    {
+        "question": "Im Zuge welcher Revolution wurde Ludwig XVI. zum Tode verurteilt und hingerichtet?",
+        "answer_1": "Amerikanische Revolution",
+        "answer_2": "Japanische Revolution",
+        "answer_3": "Französische Revolution",
+        "answer_4": "Deutsche Revolution",
+        "right": 3
+    },
+    {
+        "question": "Wer war der erste Bundeskanzler der Bundesrepublik Deutschland?",
+        "answer_1": "Willy Brandt",
+        "answer_2": "Konrad Adenauer",
+        "answer_3": "Helmut Schmidt",
+        "answer_4": "Ludwig Erhard",
+        "right": 2 
+    },
+    {
+        "question": "Was kauften die USA im Jahre 1867 vom Russischen Kaiserreich?",
+        "answer_1": "Haiti",
+        "answer_2": "Alaska",
+        "answer_3": "Malediven",
+        "answer_4": "Grönland",
+        "right": 2
+    },
+    {
+        "question": "In welchem Jahr wurde die NATO gegründet?",
+        "answer_1": "1924",
+        "answer_2": "1950",
+        "answer_3": "1949",
+        "answer_4": "1974",
+        "right": 3
+    },
+    {
+        "question": "Wer gilt als Erfinder des modernen Buchdrucks mit beweglichen Metalllettern und der Druckerpresse?",
+        "answer_1": "Johannes Gutenberg",
+        "answer_2": "Papst Franziskus",
+        "answer_3": "Ludwig Erhard",
+        "answer_4": "Gregor Mendel",
+        "right": 1
+    },
+    {
+        "question": "Wer führte 1914 ein Attentat auf Erzherzog Franz Ferdinand durch?",
+        "answer_1": "Gavrilo Princip",
+        "answer_2": "Kaiser Wilhelm II",
+        "answer_3": "Alfred Graf von Schlieffen",
+        "answer_4": "Theobald von Bethmann Hollweg",
+        "right": 1
+    },
+    {
+        "question": "Was wurde im Mittelalter zur Verhütung verwendet?",
+        "answer_1": "Naturdarm",
+        "answer_2": "Socken",
+        "answer_3": "Handschuhe",
+        "answer_4": "Es gab keine",
+        "right": 1
+    }
+];
+
+let geographyQuestions = [
+
+]
+
+let computerQuestions = [
+
+]
+
+let generalQuestions = [
+    
+]
+
+
+
+
+// {
+//     "question": "",
+//     "answer_1": "",
+//     "answer_2": "",
+//     "answer_3": "",
+//     "answer_4": "",
+//     "right": 
+// },
