e5fa28758b1fe4e0dcfd65003aa091abfc8fd095
Kevin Johr <kevin.johr@posteo.de>
2021-12-26 23:45:12 +0100

    add rubric general, geography and computer quiz

--- script/quizQuestionAndAnswers.js
+++ script/quizQuestionAndAnswers.js
@@ -1,3 +1,10 @@
+// historyQuiz line 8
+// geographyQuiz line 77
+// computerQuiz line 163
+// generalQuiz line 232
+
+
+
 let historyQuiz = [
     {
         "question": "In welchem Jahr Begann der Zweite Weltkrieg?",
@@ -21,7 +28,7 @@ let historyQuiz = [
         "answer_2": "Konrad Adenauer",
         "answer_3": "Helmut Schmidt",
         "answer_4": "Ludwig Erhard",
-        "right": 2 
+        "right": 2
     },
     {
         "question": "Was kauften die USA im Jahre 1867 vom Russischen Kaiserreich?",
@@ -65,24 +72,236 @@ let historyQuiz = [
     }
 ];
 
+
+
 let geographyQuiz = [
+    {
+        "question": "Welchen Fluss bringen die Brigach und die Breg zuweg?",
+        "answer_1": "Rhein",
+        "answer_2": "Donau",
+        "answer_3": "Isar",
+        "answer_4": "Oder",
+        "right": 2
+    },
+    {
+        "question": "Welche Stadt liegt in Nordrhein-Westfalen?",
+        "answer_1": "Plauen",
+        "answer_2": "Bitburg",
+        "answer_3": "Rheda-Wiedenbrück",
+        "answer_4": "Papenburg",
+        "right": 3
+    },
+    {
+        "question": "In welches Meer mündet der Jangtsekiang?",
+        "answer_1": "Japanisches Meer",
+        "answer_2": "Phillippinensee",
+        "answer_3": "Südchinesisches Meer",
+        "answer_4": "Ostchinesisches Meer",
+        "right": 4
+    },
+    {
+        "question": "Was gibt es wirklich?",
+        "answer_1": "Labradorsee",
+        "answer_2": "Pudeltown",
+        "answer_3": "Husky River",
+        "answer_4": "Bernhardinenberg",
+        "right": 1
+    },
+    {
+        "question": "Welches Land grenzt nicht ans Schwarze Meer?",
+        "answer_1": "Griechenland",
+        "answer_2": "Georgien",
+        "answer_3": "Bulgarien",
+        "answer_4": "Rumänien",
+        "right": 1
+    },
+    {
+        "question": "Kanada besteht aus ...",
+        "answer_1": "3 Provinzen und 10 Territorien",
+        "answer_2": "24 Bundesstaaten",
+        "answer_3": "11 Bundesstaaten und 8 Provinzen",
+        "answer_4": "10 Provinzen und 3 Territorien",
+        "right": 4
+    },
+    {
+        "question": "In welcher Wüste liegt das Death Valley?",
+        "answer_1": "Sonora-Wüste",
+        "answer_2": "Nevada-Wüste",
+        "answer_3": "Chihuahua-Wüste",
+        "answer_4": "Mojave-Wüste",
+        "right": 4
+    },
+    {
+        "question": "Welches Gewässer gibt es nicht?",
+        "answer_1": "Gelbes Meer",
+        "answer_2": "Rotes Meer",
+        "answer_3": "Grünes Meer",
+        "answer_4": "Weißes Meer",
+        "right": 3
+    },
+    {
+        "question": "Welcher Fluss fließt durch Florenz?",
+        "answer_1": "Arno",
+        "answer_2": "Flo",
+        "answer_3": "Renzo",
+        "answer_4": "Po",
+        "right": 1
+    },
+    {
+        "question": "Welche ist die südlichste Hauptstadt der Welt?",
+        "answer_1": "Kapstadt",
+        "answer_2": "Santiago de Chile",
+        "answer_3": "Wellington",
+        "answer_4": "Canberra",
+        "right": 3
+    }
 
 ]
+
+
 
 let computerQuiz = [
-
+    {
+        "question": "Wie nennt man völlig kostenlose Programme für Computer?",
+        "answer_1": "Software",
+        "answer_2": "Hardware",
+        "answer_3": "Shareware",
+       "answer_4": "Freeware",
+        "right": 4
+    },
+    {
+        "question": "Gegeben ist die Zahl 1010 im Dualsystem. Wie heißt diese Zahl im Dezimalsystem?",
+        "answer_1": "8",
+        "answer_2": "10",
+        "answer_3": "12",
+       "answer_4": "9",
+        "right": 2
+    },
+    {
+        "question": "Was bedeutet Open-Source?",
+        "answer_1": "Es handelt sich um eine gecrackte Version.",
+        "answer_2": "Der Quellcode ist zugänglich und änderbar.",
+        "answer_3": "Alles ist gratis.",
+       "answer_4": "Man kann die Software überall verwenden.",
+        "right": 2
+    },
+    {
+        "question": "Wovon spricht man, wenn ein Betriebssystem mehrere Prozesse gleichzeitig ausführen kann?",
+        "answer_1": "Multitasking",
+        "answer_2": "Multimanagement",
+        "answer_3": "Parallel Processing",
+       "answer_4": "Swapping",
+        "right": 1
+    },
+    {
+        "question": "Aus wie vielen Bits besteht ein Byte?",
+        "answer_1": "2",
+        "answer_2": "8",
+        "answer_3": "1",
+       "answer_4": "16",
+        "right": 2
+    },
+    {
+        "question": "Was ist ein Font im Zusammenhang mit Computern?",
+        "answer_1": "ein Steuergerät für Computerspiele",
+        "answer_2": "ein Simulationsprogramm",
+        "answer_3": "ein Zeichensatz",
+       "answer_4": "ein Bildarchiv",
+        "right": 3
+    },
+    {
+        "question": "Wofür steht die Abkürzung HTML in der Computertechnologie?",
+        "answer_1": "ein Betriebssystem für WAP",
+        "answer_2": "ein Komprimierungsformat für das Internet",
+        "answer_3": "eine Computerprogrammiersprache",
+       "answer_4": "eine Auszeichnungssprache zur Gestaltung von Webseiten im Internet",
+        "right": 4
+    },
+    {
+        "question": "Wie heißt die Software für die Ablaufsteuerung in einem Computer?",
+        "answer_1": "Intrasoft",
+        "answer_2": "TCP/IP",
+        "answer_3": "Betriebssystem",
+       "answer_4": "Interface",
+        "right": 3
+    }
 ]
+
+
 
 let generalQuiz = [
-    
+    {
+        "question": "Wie viele Zähne hat ein erwachsener Mensch normalerweise?",
+        "answer_1": "26",
+        "answer_2": "30",
+        "answer_3": "32",
+       "answer_4": "36",
+        "right": 3
+    },
+    {
+        "question": "Wofür steht die Abkürzung KGaA?",
+        "answer_1": "Kreditgesellschaft auf Aktien",
+        "answer_2": "Kommanditgesellschaft auf Aktien",
+        "answer_3": "Kardinalgesellschaft auf Aktien",
+       "answer_4": "Kompetenzgesellschaft auf Aktien",
+        "right": 2
+    },
+    {
+        "question": "Was ist ein Oxymoron?",
+        "answer_1": "Ein innerer Widerspruch",
+        "answer_2": "Ein Versfuß",
+        "answer_3": "Eine Wiederholung",
+       "answer_4": "Eine Frageform",
+        "right": 1
+    },
+    {
+        "question": "Wofür steht das „L“ im Sender RTL?",
+        "answer_1": "London",
+        "answer_2": "Luxembourg",
+        "answer_3": "Lichtenstein",
+       "answer_4": "Los Angeles",
+        "right": 2
+    },
+    {
+        "question": "Wie viele Oscars gewann der Film „Titanic“?",
+        "answer_1": "10",
+        "answer_2": "11",
+        "answer_3": "12",
+       "answer_4": "13",
+        "right": 2
+    },
+    {
+        "question": "Wie beginnt Artikel 1 des deutschen Grundgesetzes?",
+        "answer_1": "Alle Menschen sind vor dem Gesetz gleich",
+        "answer_2": "Jeder hat das Recht auf die freie Entfaltung seiner Persönlichkeit",
+        "answer_3": "Jeder hat das Recht, seine Meinung in Wort, Schrift und Bild frei zu äußern",
+       "answer_4": "Die Würde des Menschen ist unantastbar",
+        "right": 4
+    },
+    {
+        "question": "Wie heißt die Schicht der Atmosphäre, die der Erde am nächsten ist?",
+        "answer_1": "Troposphäre",
+        "answer_2": "Stratosphäre",
+        "answer_3": "Mesosphäre",
+       "answer_4": "Thermosphäre",
+        "right": 1
+    },
+    {
+        "question": "Welcher ist der längste innerdeutsche Fluss?",
+        "answer_1": "Rhein",
+        "answer_2": "Weser",
+        "answer_3": "Elbe",
+       "answer_4": "Donau",
+        "right": 1
+    }
 ]
 
 
-// {
-//     "question": "",
-//     "answer_1": "",
-//     "answer_2": "",
-//     "answer_3": "",
+//  {
+//      "question": "",
+//      "answer_1": "",
+//      "answer_2": "",
+//      "answer_3": "",
 //     "answer_4": "",
-//     "right": 
-// },
+//      "right":
+//  },
